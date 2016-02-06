ef4bbf4ebbc590b4a3b7840424b54af609530e3d
Maxwel Dsouza <maxellusionist@gmail.com>
2016-02-06 21:38:07 +0530

    bump version, fix license

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(name='mysqldbhelper',
     packages=['mysqldbhelper'],
-    version='1.0',
+    version='1.0.1',
     description='A simple wrapper over mysqldb',
     url='https://github.com/comparnion/mysqldbhelper',
     author='Maxwel Dsouza',
